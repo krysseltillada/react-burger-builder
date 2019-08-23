039e6604b33b375d336427b890d6ce31ab385f65
kryssel tillada <krysseltillada@gmail.com>
2019-08-23 23:26:18 +0800

    Update Burger.js

--- src/components/burger/Burger.js
+++ src/components/burger/Burger.js
@@ -13,7 +13,7 @@ const burger = (props) => {
         }, []);
 
     if(transformedIngredients.length === 0){
-        transformedIngredients = <p>Please start adding ingredients!</p>
+        transformedIngredients = <p>lol!</p>
     }
 
     return (
